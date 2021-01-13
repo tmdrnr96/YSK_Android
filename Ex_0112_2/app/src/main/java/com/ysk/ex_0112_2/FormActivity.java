d1c8effc606804c5098745ba0f51216e0660b2d7
yooseungkook <tmdrnr96@naver.com>
2021-01-13 13:25:23 +0900

    Android

---
+++ Ex_0112_2/app/src/main/java/com/ysk/ex_0112_2/FormActivity.java
@@ -0,0 +1,14 @@
+package com.ysk.ex_0112_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+}
